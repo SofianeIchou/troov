e60d07d43af3aba8dc166209cf88f76384f6890b
Sofiane <s@s.s>
2022-02-23 16:51:23 +0100

    Add Nuxt & Chakra

---
+++ layouts/default.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <CThemeProvider>
+      <CColorModeProvider>
+        <CBox font-family="body" as="main">
+          <CReset />
+          <Nuxt />
+        </CBox>
+      </CColorModeProvider>
+    </CThemeProvider>
+  </div>
+</template>
+<script>
+import {
+  CThemeProvider,
+  CColorModeProvider,
+  CReset,
+  CBox,
+  CButton,
+} from "@chakra-ui/vue";
+
+export default {
+  name: "DefaultLayout",
+  components: {
+    CThemeProvider,
+    CColorModeProvider,
+    CReset,
+    CBox,
+    CReset,
+    CButton,
+  },
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,125 @@
+<template>
+  <div class="container">
+    <CBox
+      v-bind="mainStyles[colorMode]"
+      d="flex"
+      w="100vw"
+      h="100vh"
+      flex-dir="column"
+      justify-content="center"
+    >
+      <CHeading text-align="center" mb="4"> ⚡️ Bienvenue </CHeading>
+      <CInput placeholder="Basic usage" />
+      <CInputGroup size="md">
+        <CInput
+          pr="4.5rem"
+          :type="show ? 'text' : 'password'"
+          placeholder="Enter password"
+          v-model="password"
+        />
+        <CInputRightElement width="4.5rem">
+          <CButton h="1.75rem" size="sm" @click="show = !show">
+            {{ show ? "Hide" : "Show" }}
+          </CButton>
+        </CInputRightElement>
+      </CInputGroup>
+      <CFlex justify="center" direction="column" align="center">
+        <CBox mb="3">
+          <CIconButton
+            mr="3"
+            :icon="colorMode === 'light' ? 'moon' : 'sun'"
+            :aria-label="`Switch to ${
+              colorMode === 'light' ? 'dark' : 'light'
+            } mode`"
+            @click="toggleColorMode"
+          />
+          <CButton left-icon="info" variant-color="blue" @click="showToast">
+            Inscription
+          </CButton>
+        </CBox>
+      </CFlex>
+    </CBox>
+  </div>
+</template>
+
+<script lang="js">
+import {
+  CBox,
+  CButton,
+  CAvatarGroup,
+  CAvatar,
+  CAvatarBadge,
+  CModal,
+  CModalContent,
+  CModalOverlay,
+  CModalHeader,
+  CModalFooter,
+  CModalBody,
+  CModalCloseButton,
+  CIconButton,
+  CFlex,
+  CHeading,
+  CInput
+} from '@chakra-ui/vue'
+
+export default {
+  name: 'IndexPage',
+  components: {
+    CBox,
+    CButton,
+    CAvatarGroup,
+    CAvatar,
+    CAvatarBadge,
+    CModal,
+    CModalContent,
+    CModalOverlay,
+    CModalHeader,
+    CModalFooter,
+    CModalBody,
+    CModalCloseButton,
+    CIconButton,
+    CFlex,
+    CHeading
+  },
+  inject: ['$chakraColorMode', '$toggleColorMode'],
+  data () {
+    return {
+      password: ' ',
+      show: false,
+      showModal: false,
+      mainStyles: {
+        dark: {
+          bg: 'gray.700',
+          color: 'whiteAlpha.900'
+        },
+        light: {
+          bg: 'white',
+          color: 'gray.900'
+        }
+      }
+    }
+  },
+  computed: {
+    colorMode () {
+      return this.$chakraColorMode()
+    },
+    theme () {
+      return this.$chakraTheme()
+    },
+    toggleColorMode () {
+      return this.$toggleColorMode
+    }
+  },
+  methods: {
+    showToast () {
+      this.$toast({
+        title: 'Account created.',
+        description: "We've created your account for you.",
+        status: 'success',
+        duration: 10000,
+        isClosable: true
+      })
+    }
+  }
+}
+</script>
